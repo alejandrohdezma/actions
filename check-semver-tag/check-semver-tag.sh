e7ceb1a6b918a672469a7f2d71fc08ce261ac170
Alejandro Hernández <info@alejandrohdezma.com>
2024-04-23 16:32:52 +0200

    Ensure `check-semver-tag` fails if tag doesn't follow semver

--- check-semver-tag/check-semver-tag.sh
+++ check-semver-tag/check-semver-tag.sh
@@ -1,5 +1,6 @@
 if [[ $TAG =~ ^(refs/tags/)?v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     echo "🏷 $TAG follows semver"
 else
-    echo "::error ::Tag $TAG does not follow the pattern 'vX.X.X'"
+    echo "::error ::Tag $TAG does not follow semver"
+    exit 1
 fi
